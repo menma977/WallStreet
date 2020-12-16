63f104d314683c82f98ffa565445ca1d1f1e6d30
SusuTawar <realsusutawar@gmail.com>
2020-12-17 04:49:30 +0700

    home & background service for balance

--- app/src/main/java/info/wallstreet/view/fragment/HomeFragment.kt
+++ app/src/main/java/info/wallstreet/view/fragment/HomeFragment.kt
@@ -5,10 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import info.wallstreet.R
+import info.wallstreet.model.User
+import info.wallstreet.view.NavigationActivity
 
 class HomeFragment : Fragment() {
+  private lateinit var user:User
+  private lateinit var btc:TextView
+  private lateinit var ltc:TextView
+  private lateinit var eth:TextView
+  private lateinit var doge:TextView
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_home, container, false)
+    val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+    user = User(activity as NavigationActivity)
+
+    btc = view.findViewById(R.id.textViewBitcoinBalance)
+    ltc = view.findViewById(R.id.textViewLitecoinBalance)
+    eth = view.findViewById(R.id.textViewEthereumBalance)
+    doge = view.findViewById(R.id.textViewDogecoinBalance)
+
+    btc.text = user.getString("balance_btc")
+    ltc.text = user.getString("balance_ltc")
+    eth.text = user.getString("balance_eth")
+    doge.text = user.getString("balance_doge")
+
+    return view
   }
 }
