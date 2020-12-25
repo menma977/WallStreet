442478f7ed8c0bd69cf52bf9c4279a331d48806e
SmityWaberJagerManJanson <com.owl.minerva@gmail.com>
2020-12-25 14:10:09 +0700

    Update CamelController.kt

--- app/src/main/java/info/wallstreet/controller/CamelController.kt
+++ app/src/main/java/info/wallstreet/controller/CamelController.kt
@@ -1,22 +1,20 @@
 package info.wallstreet.controller
 
 import info.wallstreet.model.Url
-import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.util.concurrent.Callable
 import java.util.concurrent.TimeUnit
 
 object CamelController {
-  private fun failResponseHandler(content: JSONObject): JSONObject{
-    return JSONObject("{code:500, data:\"ERROR MESSAGE: Change Me\"}")
+  private fun failResponseHandler(content: JSONObject): JSONObject {
+    return JSONObject().put("code", 500).put("data", "ERROR MESSAGE: Change Me")
   }
 
-  private fun get(url:String): JSONObject{
+  private fun get(url: String): JSONObject {
     val client = OkHttpClient.Builder()
     client.connectTimeout(30, TimeUnit.SECONDS)
     client.writeTimeout(30, TimeUnit.SECONDS)
@@ -32,24 +30,22 @@ object CamelController {
     val inputReader = BufferedReader(InputStreamReader(response.body!!.byteStream()))
     var rawContent = ""
     var line = inputReader.readLine()
-    while(line != null){
+    while (line != null) {
       rawContent += line
       line = inputReader.readLine()
     }
     val content = JSONObject(rawContent)
     return when {
-      response.isSuccessful ->
-        JSONObject("{code: 200}").put("data", content)
-      else ->
-        failResponseHandler(content)
+      response.isSuccessful -> JSONObject().put("code", 200).put("data", content)
+      else -> failResponseHandler(content)
     }
   }
 
-  public fun getBalance(address: String): JSONObject {
+  fun getBalance(address: String): JSONObject {
     return get(Url.camel("/getbalance/$address"))
   }
 
-  public fun getTokenBalance(address: String): JSONObject {
+  fun getTokenBalance(address: String): JSONObject {
     return get(Url.camel("/gettokenbalance/$address"))
   }
 
